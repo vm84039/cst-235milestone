8be067f42c14d2a875f611b4321fb4729802bb4d
vm84039 <69450472+vm84039@users.noreply.github.com>
2021-05-29 09:54:20 -0700

    update2

--- src/main/java/beans/User.java
+++ src/main/java/beans/User.java
@@ -2,25 +2,38 @@ package beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
 
 @ManagedBean
 @ViewScoped 
 public class User {
+	@NotNull() @Size(min=5, max=15)
 	private String firstName;
+	@NotNull() @Size(min=5, max=15)
 	private String lastName;
+	@NotNull() @Min(1) @Max(12)
 	private int month;
+	@NotNull() @Min(1) @Max(31)
 	private int day;
+	@NotNull()@Min(1900) @Max(2010)
 	private int year;
+	@NotNull() @Email @Size(min=5, max=35)
 	private String emailAddress;
+	@NotNull() @Size(min=5, max=15)
 	private String password;
 	
 
 	public User()
 	{
-        this.firstName = "John";
-        this.lastName = "Doe";
-        this.emailAddress = "JDoe@gmail.com";
-        this.password = "abc123";
+        this.firstName = "";
+        this.lastName = "";
+        this.emailAddress = "";
+        this.password = "";
         this.year = 1990;
         this.month = 1;
         this.day = 1;
