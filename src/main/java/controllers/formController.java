eced913293a7340808bc5638b538ebf807db2d5f
vm84039 <69450472+vm84039@users.noreply.github.com>
2021-05-29 00:36:27 -0700

    update

---
+++ src/main/java/controllers/formController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import beans.User;
+
+@ManagedBean
+@ViewScoped
+
+public class formController {
+	
+	public String registrationSubmit(User user) 
+	{
+		FacesContext context = FacesContext.getCurrentInstance();
+		//put the user object into the POST request
+		user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		return "registrationSuccess.xhtml";
+	}
+	public String Authenticate(User user) 
+	{
+		FacesContext context = FacesContext.getCurrentInstance();
+		//put the user object into the POST request
+		user = context.getApplication().evaluateExpressionGet(context, "#{user}", User.class);
+		FacesContext.getCurrentInstance().getExternalContext().getRequestMap().put("user", user);
+		return "CustomerLoginSuccess.xhtml";
+	}
+	
+	
+
+}
